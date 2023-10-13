cd20e93cc119e103db5cfc11723e8dc3fd21451c
jasonandjay <342690199@qq.com>
2023-10-13 17:08:20 +0800

    ✨feature: update snap remove address from getAllXpub

--- packages/snap/src/rpc/getAllXpubs.ts
+++ packages/snap/src/rpc/getAllXpubs.ts
@@ -1,68 +1,55 @@
 import { panel, text, heading } from '@metamask/snaps-ui';
-import { networks, address } from 'bitcoinjs-lib';
+import { networks } from 'bitcoinjs-lib';
 import { BitcoinNetwork, ScriptType, Snap } from '../interface';
 import { convertXpub } from '../bitcoin/xpubConverter';
 import { RequestErrors, SnapError } from '../errors';
 import { getHDRootNode } from '../bitcoin/hdKeyring';
-import { getAddress } from '../bitcoin/simpleKeyring';
-interface Account{
+interface Account {
   xpub: string;
   scriptType: ScriptType;
   network: BitcoinNetwork;
-  address: string;
 }
 
-export async function getAllXpubs(origin: string, snap: Snap): Promise<{xpubs: string[], accounts: {}, mfp: string}> {
+export async function getAllXpubs(origin: string, snap: Snap): Promise<{ xpubs: string[], accounts: {}, mfp: string }> {
   const result = await snap.request({
     method: 'snap_dialog',
     params: {
       type: 'confirmation',
       content: panel([
         heading('Access your extended public key'),
-        text(`${origin} is trying to access your Bitcoin Legacy, SegWit and Native SegWit extended public keys.`),
+        text(`${origin} is trying to access your Bitcoin Legacy, SegWit, TapRoot and Native SegWit extended public keys.`),
       ]),
     },
   });
 
   if (result) {
-    try{
-      let xfp = '';
-      const xpubs: string[] = [];
-      const accounts: Array<Account>=[];
-      const xpubsInNetworks = await Promise.all(Object.values(BitcoinNetwork).map(async (bitcoinNetwork: BitcoinNetwork) => {
-        const network = bitcoinNetwork === BitcoinNetwork.Main ? networks.bitcoin : networks.testnet;
-        const account = await Promise.all(Object.values(ScriptType).map(async (scriptType: ScriptType) => {
-          const { node: accountNode, mfp } = await getHDRootNode(snap, network, scriptType);
-          xfp = xfp || mfp;
-          const extendedPublicKey = accountNode.neutered();
+    let xfp = '';
+    const xpubs: string[] = [];
+    const accounts: Array<Account> = [];
+    await Promise.all(Object.values(BitcoinNetwork).map(async (bitcoinNetwork: BitcoinNetwork) => {
+      const network = bitcoinNetwork === BitcoinNetwork.Main ? networks.bitcoin : networks.testnet;
+      await Promise.all(Object.values(ScriptType).map(async (scriptType: ScriptType) => {
+        const { node: accountNode, mfp } = await getHDRootNode(snap, network, scriptType);
+        xfp = xfp || mfp;
+        const extendedPublicKey = accountNode.neutered();
 
-          const address = getAddress(
-            network,
-            accountNode.publicKey.toString('hex'),
-            scriptType,
-          ) as string;
-          
-          let xpub = extendedPublicKey.toBase58();
-          if (scriptType !== ScriptType.P2TR) {
-              xpub = convertXpub(xpub, scriptType, network);
-          }
-          xpubs.push(xpub);
-          accounts.push({
-            xpub,
-            scriptType,
-            network: bitcoinNetwork,
-            address,
-          })
-        }));
+        let xpub = extendedPublicKey.toBase58();
+        if (scriptType !== ScriptType.P2TR) {
+          xpub = convertXpub(xpub, scriptType, network);
+        }
+        xpubs.push(xpub);
+        accounts.push({
+          xpub,
+          scriptType,
+          network: bitcoinNetwork,
+        })
       }));
-      return {
-        mfp: xfp,
-        xpubs,
-        accounts,
-      };
-    }catch(e){
-      console.log('e...', e);
-    }
+    }));
+    return {
+      mfp: xfp,
+      xpubs,
+      accounts,
+    };
   }
   throw SnapError.of(RequestErrors.RejectKey);
 }
